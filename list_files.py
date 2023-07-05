e08322d6e4ee4a302294cf463ae25299cb4c70a4
Sineth23 <57430821+Sineth23@users.noreply.github.com>
2023-07-04 22:54:05 -0400

    Update list_files.py

--- list_files.py
+++ list_files.py
@@ -1,18 +1,11 @@
 import os
 
-def list_files():
+
+def list_files(directory):
     # Specify the directory path
-    directory = "created documentation"
+    directory = f"/home/ec2-user/{directory}/.autodoc/docs/markdown"
     
     # Get the list of files in the directory
     files = os.listdir(directory)
-    
-    # Display the list of files with numbers
-    for i, file in enumerate(files):
-        print(f"{i+1}. {file}")
-    
-    # Ask the user to choose a file by entering its number
-    file_num = int(input("Enter the number of the file you want to choose: "))
-    
-    # Return the selected file
-    return files[file_num - 1]
+ 
+    return files
